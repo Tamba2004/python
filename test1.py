25993e1eb4e8289da17daf869bdc739a17f69f24
Alan Soh <alansoh343@gmail.com>
2026-09-10 00:34:15 -0400

    Refactor comments and clean up code in test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,20 @@
-print(" j apprend le python ")
+#print(" j apprend le python ")
 # mon premier commentaire 
-if 200 > 100 : 
-    print(" true")
+#if 200 > 100 :
+    #print("true")
 
-# 2 
-nom = "sara" 
+nom_famille = "wilfried "
+print("nom de famille est " + nom_famille)
+
+# les types de donnees
+
+nbre = 20 
+nbre_1 = 15 
+print (nbre + nbre_1)
+
+# chaine de caractere 
+jour  = "mardi"
+print(" ce jour de la semaine est " + jour)
+
+
+
